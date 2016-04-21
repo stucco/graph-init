8f59920c84d8c8a69d1c3235581863c4d99b453a
euf <euf@ornl.gov>
2016-04-21 17:14:47 -0400

    remove unneeded use of ontology repo

--- schema-build.sh
+++ schema-build.sh
@@ -2,9 +2,6 @@
 
 echo "Checking out modules..."
 mvn -q --non-recursive scm:checkout -Dmodule.name=graph-db-connection -Dbranch.name=master
-git clone -b master https://github.com/stucco/ontology.git
-cp ontology/stucco_schema.json .
-cp graph-db-connection/*.yml .
 
 echo "Building graph-init..."
 mvn clean install -Dmaven.test.skip=true
@@ -14,5 +11,4 @@ cd ..
 
 echo "Cleaning up...."
 rm -rf graph-db-connection
-rm -rf ontology
 
