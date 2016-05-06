4e02ee8b9f29799fc2f9d4b1abd9f94346717109
euf <euf@ornl.gov>
2016-05-06 18:29:55 -0400

    modify script and pom file to install/use ver 1.0.0 of DBConn

--- schema-build.sh
+++ schema-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Checking out modules..."
-mvn -q --non-recursive scm:checkout -Dmodule.name=graph-db-connection -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-db-connection -DscmVersion=1.0.0 -DscmVersionType=tag
 
 echo "Building graph-init..."
 mvn clean install -Dmaven.test.skip=true
